64bc025f0378be92cb79517d31178fa893aeb624
Prantick Deb <prantickdeb2005@gmail.com>
2025-11-16 01:11:52 +0530

    Components-setup

---
+++ vite-project/src/components/index.js
@@ -0,0 +1,9 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./About";
+import Contact from "./Contact";
+import Hero from "./Hero";
+import Products from "./Products";
+import Highlights from "./Highlights";
+
+export { Header, Footer, About, Contact, Hero, Products, Highlights };
