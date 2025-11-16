f24753647f630c1c92914e5b27fff460d95128cd
Prantick Deb <prantickdeb2005@gmail.com>
2025-11-16 13:50:46 +0530

    Containing the components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
-
-import viteLogo from '../medias/vite.svg'
-
-
+import { useState,useEffect } from 'react'
+import {Header,Footer,About,Contact,Hero,Products,Highlights} from './components/index'
 function App() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setScrolled(window.scrollY > 50);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return(
-    <div className="">Hey</div>
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+      <Header scrolled={scrolled} />
+      <Hero />
+      <Products />
+      <About />
+      <Highlights />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 export default App
